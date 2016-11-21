5f416960ed6ecb60eabbdff53cf74257aa9044ef
lixian <lixian@didichuxing.com>
2016-11-21 20:02:55 +0800

    begin

---
+++ github_star_sum_of_user.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'DeveloperLx'
+
+import sys, json
+from urllib import request
+
+if (len(sys.argv) < 2):
+    print('请给出您的 GitHub ID 作为参数')
+    exit()
+
+github_id = sys.argv[1]
+if (len(github_id) <= 0):
+    print('请给出您的 GitHub ID 作为参数')
+    exit()
+
+print('（查询中...）')
+
+PER_PAGE_REPO_MAX_COUNT = 30
+cur_page = 1
+total_repo = 0
+total_star = 0
+
+while True:
+
+    url = 'https://api.github.com/users/%s/repos?page=%s' % (github_id, cur_page)
+
+    with request.urlopen(url) as f:
+        data = f.read()
+
+        repo_json = data.decode('utf-8')
+        repo_array = json.loads(repo_json)
+        if (type(repo_array) != list):
+            break
+
+        for repo in repo_array:
+            if (type(repo) != dict):
+                continue
+            total_repo += 1
+            start = repo['stargazers_count']
+            total_star += start
+
+        if (len(repo_array) < PER_PAGE_REPO_MAX_COUNT):
+            break
+
+        cur_page += 1
+
+if (total_repo == 0 and total_star == 0):
+    print('=== %s，您的GitHub共有 0 个repo，0 个star (您的GitHub ID 敲错了？？) ===' % github_id)
+else:
+    print('=== %s，您的GitHub共有 %s 个repo，%s 个star ===' % (github_id, total_repo, total_star))
